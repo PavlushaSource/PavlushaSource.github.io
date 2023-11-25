278e45659aa67ec643348c1bbf6a47672b98848f
PavlushaSource <pavlushkin.dima1785@gmail.com>
2023-11-25 22:41:22 +0300

    feat: Сhanged a lot of things on the site for me personally

--- resume.py
+++ resume.py
@@ -5,8 +5,9 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__, template_folder="templates")
 
+
 @app.route("/")
-def hello_world():
+def start_server():
     return render_template("index.html")
 
 

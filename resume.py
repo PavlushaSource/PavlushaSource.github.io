d667a4cf8ff5e2e15ec478e3dd254dd5eb68bc12
PavlushaSource <pavlushkin.dima1785@gmail.com>
2023-11-25 19:19:23 +0300

    fix: Change template logic for working js scripts

--- resume.py
+++ resume.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for
+
 # from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy.orm import DeclarativeBase
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
